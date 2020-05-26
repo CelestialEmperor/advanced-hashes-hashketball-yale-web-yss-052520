3d188099e367c93a84fe7829135a51baf29c3310
Jack Ma <mapeiyao@gmail.com>
2020-05-26 20:52:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -193,9 +193,8 @@ def big_shoe_rebounds
   player_size = {}
   game_hash.each do |home_away, team_info|
     team_info[:players].each do |player_hash|
-        if player_hash[:player_name] == player_name
-          return player_hash
-        end
+      if player_hash[:player_name] == player_name
+        return player_hash
       end
     end
   end
